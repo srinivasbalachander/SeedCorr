aac06e7eeda7b282cf97c1f53f0975f9be39adfb
Srinivas Balachander <55076917+srinivasbalachander@users.noreply.github.com>
2024-05-05 08:21:11 +0530

    Update dlpfc_anova.py

--- dlpfc_anova.py
+++ dlpfc_anova.py
@@ -1,4 +1,10 @@
-# Seed Based Correlation 2nd Level (Group) Analysis using NiLearn
+# 2nd Level (Group) Analysis for CRC-Neuromodulation MRIs using NiLearn
+
+# Specify important variables about the analysis (see if these can be entered from the command line)
+
+halfpipe_deriv = "/mnt/HPStorage/CRC_Analysis/Analysis/derivatives/halfpipe/"
+firstlevel_name = "seedCorr1"
+roi = "LDLPFC"
 
 # Load the necessary libraries
 from pathlib import Path
@@ -13,28 +19,22 @@ from nilearn.glm.second_level import SecondLevelModel
 from nilearn.glm import threshold_stats_img
 from nilearn.reporting import make_glm_report
 
-# Specify important variables about the analysis
-
-halfpipe_deriv = "/mnt/HPStorage/CRC_Analysis/Analysis/derivatives/halfpipe/"
-motion_correct_method = "seedCorr1"
-roi = "LDLPFC"
-
 # Get paths to seed correlation maps 
 
-corrmap_paths = glob.glob(halfpipe_deriv + "*" + 
+firstlevel_paths = glob.glob(halfpipe_deriv + "*" + 
                           "/ses-10/func/task-rest/" + "*" + 
-                          motion_correct_method + "*" + 
+                          firstlevel_name + "*" + 
                           roi + "*" + "z_statmap.nii.gz")
 
 nsubj = len(corrmap_paths)
 
 print("\nStarting second level analysis for seed-based correlation from " + 
-      roi + " using " + motion_correct_method + " images")
+      roi + " using " + firstlevel_name + " images")
 
 print("\nNumber of subjects for this analysis = ", nsubj)
 
 # Get list of subjects
-subjlist = [re.search("sub-(.*?)/ses", i).group(1) for i in corrmap_paths]
+subjlist = [re.search("sub-(.*?)/ses", i).group(1) for i in firstlevel_paths]
 
 print("\nList of subjects included for this analysis: \n")
 print(*subjlist, sep = '\n')
@@ -45,7 +45,7 @@ df = pd.read_csv("covs/covs.csv")
 print("Here's the raw covariates file, check if its OK\n\n")
 print(df)
 
-#--------------------Make covariates design-matrix worthy
+#------------------- Make covariates design-matrix worthy
 
 # Check duplicates in the covariates file
 unique, counts = np.unique(df[['ID']], return_counts=True)
@@ -88,18 +88,19 @@ print("\nHere's the design matrix now..\n\n")
 print(full_matrix)  
 
 #------------------ Fit the statistical models
+
 print("\nFitting the main GLM models.. \n\n This might take a while so go grab a coffee while you can!")
 
 # Suffix '0' is for null/intercept-only model, '1' for Group-Only model and '2' for full model adjusted for age/gender
 
 print("\n\tNow fitting the Intercept-only (null) model... \n")
-second_level_model0 = SecondLevelModel(n_jobs=8).fit(corrmap_paths, design_matrix = null_matrix)
+second_level_model0 = SecondLevelModel(n_jobs=8).fit(firstlevel_paths, design_matrix = null_matrix)
 
 print("\n\tNow fitting the Group-only (unadjusted) model... \n")
-second_level_model1 = SecondLevelModel(n_jobs=8).fit(corrmap_paths, design_matrix = grouponly_matrix)
+second_level_model1 = SecondLevelModel(n_jobs=8).fit(firstlevel_paths, design_matrix = grouponly_matrix)
 
 print("\n\tNow fitting the Full (Age & Gender adjusted) model... \n")
-second_level_model2 = SecondLevelModel(n_jobs=8).fit(corrmap_paths, design_matrix = covs)
+second_level_model2 = SecondLevelModel(n_jobs=8).fit(firstlevel_paths, design_matrix = covs)
 
 # Apply contrasts
 print("\nModel fitting steps completed successfully, applying various contrasts now.. \n")
@@ -130,12 +131,12 @@ for i in zmap_names:
     z_maps[i] = eval(i) 
 
 # Save them to the ouput directory
-output_dir = Path.cwd() / "results" / roi
+output_dir = Path.cwd() / "results" / roi / firstlevel_name
 output_dir.mkdir(exist_ok=True, parents=True)
 
 # Run a for loop to save all z_maps to the output directory
 for i in zmap_names:
-  z_maps[i].to_filename(Path(output_dir, i + "_" + roi + "_" + motion_correct_method + '.nii.gz'))
+  z_maps[i].to_filename(Path(output_dir, i + "_" + roi + "_" + firstlevel_name + '.nii.gz'))
 
 # Save GLM reports as html files
 icbm152_2009 = datasets.fetch_icbm152_2009()
@@ -152,15 +153,15 @@ report2 = make_glm_report(model=second_level_model2, contrasts = ['SZP', 'DEP', 
                           height_control='fdr', alpha = 0.05, cluster_threshold=10,
                           title = "Full (age & gender adjusted) model")
 
-report0.save_as_html(Path(output_dir, "NullModel" + "_" + roi + "_" + motion_correct_method + '.html'))
-report1.save_as_html(Path(output_dir, "GroupOnly" + "_" + roi + "_" + motion_correct_method + '.html'))
-report2.save_as_html(Path(output_dir, "FullModel" + "_" + roi + "_" + motion_correct_method + '.html'))
+report0.save_as_html(Path(output_dir, "NullModel" + "_" + roi + "_" + firstlevel_name + '.html'))
+report1.save_as_html(Path(output_dir, "GroupOnly" + "_" + roi + "_" + firstlevel_name + '.html'))
+report2.save_as_html(Path(output_dir, "FullModel" + "_" + roi + "_" + firstlevel_name + '.html'))
 
 # Saving a few other important info
 
 import json
 
-motion_int = re.findall('\d+', motion_correct_method)
+motion_int = re.findall('\d+', firstlevel_name)
 
 if (motion_int == 1)  : 
   motion_correct_method = "aCompCor"
@@ -175,7 +176,7 @@ description = {'SeedRegion': roi,
                 'N_SZP' : int(covs['SZP'].sum()),
                 'N_DEP' : int(covs['DEP'].sum()),
                 'N_HC' : int(covs['HC'].sum()),
-                'subjpaths': corrmap_paths}
+                'subjpaths': firstlevel_paths}
 
 with open(Path(output_dir, "description" + "_" + roi + "_" + motion_correct_method + ".json"), "a") as outfile :
     outfile.write(json.dumps(description, indent = 2))
